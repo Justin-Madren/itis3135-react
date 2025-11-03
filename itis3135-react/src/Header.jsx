940dfca34b49128c1fb089c98229fd02dcf5613d
Justin-Madren <justinmadren765@gmail.com>
2025-11-03 18:21:00 -0500

    Creation

---
+++ itis3135-react/src/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header(){
+    return <header>
+    <h1>Justin Madren's Jovial Mountain lion || ITIS - 3135</h1>
+    <nav>
+        <a href="index.html">Home </a> ||
+        <a href="introduction.html"> Introduction </a> ||
+        <a href="contract.html"> Contract </a> ||
+        <a href="website_evaluations.html"> CRAP Webpage Evaluations </a> ||
+        <a href="fccfsjs_outline.html"> JavaScript Curriculum </a>||
+        <a href="formsubmition.html"> Form Submition</a>
+    </nav>
+    </header>;
+}
